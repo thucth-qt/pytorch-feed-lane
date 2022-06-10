195b63ae616dadf760d23d5058f34e492e0d55b9
Your Name <you@example.com>
2022-06-10 17:54:38 +0000

    first commit

--- main.py
+++ main.py
@@ -39,18 +39,28 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
+# import pdb; pdb.set_trace()
+# trainset = torchvision.datasets.CIFAR10(
+#     root='./data', train=True, download=True, transform=transform_train)
+# trainloader = torch.utils.data.DataLoader(
+#     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
+# testset = torchvision.datasets.CIFAR10(
+#     root='./data', train=False, download=True, transform=transform_test)
+# testloader = torch.utils.data.DataLoader(
+#     testset, batch_size=100, shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+# classes = ('plane', 'car', 'bird', 'cat', 'deer',
+#            'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+class_names= ["empty", "minimal", "normal", "full"]
+import .dataset
+trainset = dataset.FeedLaneDataset(class_names=class_names,\
+                            root_dir="/content/pytorch-feed-lane/data/subdata/train",\
+                            transform=transform_train)
+# trainloader = torch.utils.data.DataLoader(
+#     trainset, batch_size=128, shuffle=True, num_workers=2)
 
 # Model
 print('==> Building model..')
